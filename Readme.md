5a48bae16b12648482c6266393018891976d2824
sashrikazelar <sashrika@zelarsoft.com>
2025-07-14 15:36:00 +0530

    counter program,constants

--- Readme.md
+++ Readme.md
@@ -15,3 +15,18 @@
      2.by implementing HTLM textbox
 
 4.Typeconversion = change dataype of a value to another datatype
+  let x="sashrika" , x=Number(x)  console.log(x,typeof x)
+  let y= "pizza"   ,  y=string(y)   console.log(x,typeof x)
+  let z="sashrika" , z=Number(z)  console.log(z,typeof z) like this we can convert into another datatype 
+
+5. consts in javscript is the value that cannot be changes 
+
+6.counter program html,css and JS
+
+7. MATH methods 
+   x=3.21,y=2.89 , z=9
+   Math.round(x)=3, Math.floor()=3,Math.celi()=4, Math.sin,cos,tan(), Math.abs(),Math.sign(),Math.sqrt(),
+   Math.log(), Math.random()
+   Math.max(x,y,z),Math.min(x,y,z)
+
+8.RANDOM NUMBER GENERATOR
--- index.js
+++ index.js
@@ -3,12 +3,39 @@
 //1.window.prompt
 //2.by implementing HTLM textbox
 
-let username
+// let username
 
 // username= window.prompt("Enter your username")
 // console.log(username)
-document.getElementById("btn").onclick= function(){
-    username= document.getElementById("input").value
-    document.getElementById("myid").textContent=`hello ${username}`
+// document.getElementById("btn").onclick= function(){
+//     username= document.getElementById("input").value
+//     document.getElementById("myid").textContent=`hello ${username}`
 
-}
+// }
+
+// COUNTER PROGRAM
+
+// const decbtn= document.getElementById("decreasebtn")
+// const incbtn= document.getElementById("increasebtn")
+// const resetbtn= document.getElementById("resetbtn")
+// const count=document.getElementById("count")
+
+// let countvalue=0
+
+// incbtn.onclick=function(){
+//     countvalue++
+//     count.textContent=countvalue
+// }
+
+// decbtn.onclick=function(){
+//     countvalue--
+//     count.textContent=countvalue
+// }
+
+// resetbtn.onclick=function(){
+//     countvalue=0
+//     count.textContent=countvalue
+// }
+
+
+
